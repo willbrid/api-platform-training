99eb4013fbc3afe168d949b9787b8dc7076b0fff
ngaswilly77 <ngaswilly77@gmail.com>
2020-12-27 06:28:36 +0100

    feat: add validation

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -13,6 +13,7 @@ use Carbon\Carbon;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Serializer\Annotation\SerializedName;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ApiResource(
@@ -47,12 +48,21 @@ class CheeseListing
     /**
      * @ORM\Column(type="string", length=255)
      * @Groups({"cheese_listing:read", "cheese_listing:write"})
+     *
+     * @Assert\NotBlank()
+     * @Assert\Length(
+     *     min="2",
+     *     max="50",
+     *     maxMessage="Describe your cheese in 50 chars or less"
+     * )
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
      * @Groups({"cheese_listing:read"})
+     *
+     * @Assert\NotBlank()
      */
     private $description;
 
@@ -61,6 +71,9 @@ class CheeseListing
      *
      * @ORM\Column(type="integer")
      * @Groups({"cheese_listing:read", "cheese_listing:write"})
+     *
+     * @Assert\NotBlank()
+     * @Assert\GreaterThan(value="0")
      */
     private $price;
 
