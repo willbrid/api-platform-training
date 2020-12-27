9c93fc62b8024c39919bb11cfb23926a04dd3dac
ngaswilly77 <ngaswilly77@gmail.com>
2020-12-27 04:30:07 +0100

    refactor: add carbon bundle, add textDescription setter, remove setCreatedAt methode, define getCreatedAtAgo

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\CheeseListingRepository;
+use Carbon\Carbon;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -53,6 +54,11 @@ class CheeseListing
      */
     private $isPublished;
 
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -75,9 +81,9 @@ class CheeseListing
         return $this->description;
     }
 
-    public function setDescription(?string $description): self
+    public function setTextDescription(string $description): self
     {
-        $this->description = $description;
+        $this->description = nl2br($description);
 
         return $this;
     }
@@ -99,11 +105,9 @@ class CheeseListing
         return $this->createdAt;
     }
 
-    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    public function getCreatedAtAgo(): string
     {
-        $this->createdAt = $createdAt;
-
-        return $this;
+        return Carbon::instance($this->getCreatedAt())->diffForHumans();
     }
 
     public function getIsPublished(): ?bool
